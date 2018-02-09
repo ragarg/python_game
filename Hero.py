e1c8caa72f9d84beca8251836bc3cc44abb110fe
ragarg <trall_99@mail.ru>
2018-02-09 16:17:51 +0300

    Update Hero.py

--- Hero.py
+++ Hero.py
@@ -5,11 +5,7 @@ class Hero:
     def __init__(self, x, y, image, ObjectAtrtibutes):
         DinamicObject.DinamicObject.__init__(1, x, y, image, ObjectAtrtibutes)
         self.EXP = 0
-
-    def MoveHero(self, move_x, move_y):
-        self.x = move_x
-        self.y = move_y
-
+        
     def customization(self, image):
         self.image  =  image
 

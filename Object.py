1112e76d9d0fbfda6c5bf12689acf17308a2ecb9
ragarg <trall_99@mail.ru>
2018-02-09 15:06:20 +0300

    Update Object.py

--- Object.py
+++ Object.py
@@ -1,19 +1,7 @@
-import pygame
-
 class Object:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.image = None
-        self.image_rect = None
-        self.roomID = 0
-        
     def __init__(self, x, y, image):
-        self.x = x
-        self.y = y
-        self.image = image
-        self.image_rect = self.image.get_rect()
-        self.image_rect.move(x, y)
+        SetCoordinates(x, y)
+        SetImage(image)
         self.roomID = 0
 
     def SetImage(self, image):
@@ -26,7 +14,7 @@ class Object:
     def SetRoomID(self, roomnumber):
         roomID = roomnumber
 
-    def GetImage(self,):
+    def GetImage(self):
         return self.image
 
     def GetCoordinates(self):

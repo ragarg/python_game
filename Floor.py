385effb85a63008efb3d954c00faffa0f92e6d3c
ragarg <trall_99@mail.ru>
2018-02-10 15:32:09 +0300

    Update Floor.py

--- Floor.py
+++ Floor.py
@@ -4,6 +4,7 @@ class Floor(Object.Object):
     def __init__(self, x, y, image):
         Object.Object.__init__(x, y, image)
         self.size = (24, 24)
+        self.patency = 0
 
     def GetSize(self):
         return self.size

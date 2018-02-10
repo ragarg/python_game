d51d4d9961deaa68be14e1b0d7218b68e1fbc231
ragarg <trall_99@mail.ru>
2018-02-10 15:26:21 +0300

    Add files via upload

---
+++ LocationGraf.py
@@ -0,0 +1,34 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.neighbors = []
+
+    def add_new_neighbors(self, point):
+        self.neighbors.append(point)
+
+class LocationGraf:
+    def __init__(self, field):
+        self.graf = [[None for i in range(field.size[1])] for i in range(field.size[0])]
+        for x in range(field.size[0]):
+            for y in range(1, field.size[1] + 1):
+                self.graf[x][y] = Point(x, y)
+        for x in range(field.size[0]):
+            for y in range(1, field.size[1] + 1):
+
+                if x != 0 and field.field[x - 1][y].patency():
+                    self.graf[x][y].add_new_neighbors(self.graf[x - 1][y])
+                    if y != 0 and field.field[x - 1][y - 1].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x - 1][y - 1])
+                    if y != field.size[1] - 1 and field.field[x - 1][y + 1].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x - 1][y + 1])
+
+                if x != field.size[0] - 1 and field.field[x + 1][y].patency():
+                    self.graf[x][y].add_new_neighbors(self.graf[x + 1][y])
+                    if y != 0 and field.field[x + 1][y - 1].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x + 1][y - 1])
+                    if y != field.size[1] - 1 and field.field[x + 1][y + 1].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x + 1][y + 1])
+
+    def way(self, point_start, point_end):
+        pass

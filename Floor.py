a5277f3b4454dcf62e534980cf22d0c435c44eca
ragarg <trall_99@mail.ru>
2018-02-10 14:37:31 +0300

    Add files via upload

---
+++ Floor.py
@@ -0,0 +1,9 @@
+import Object
+
+class Floor(Object.Object):
+    def __init__(self, x, y, image):
+        Object.Object.__init__(x, y, image)
+        self.size = (24, 24)
+
+    def GetSize(self):
+        return self.size

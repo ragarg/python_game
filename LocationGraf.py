e0c497501a425c9027922d4ebd802ed72e0df19b
ragarg <trall_99@mail.ru>
2018-02-10 15:31:08 +0300

    Update LocationGraf.py

--- LocationGraf.py
+++ LocationGraf.py
@@ -16,19 +16,25 @@ class LocationGraf:
         for x in range(field.size[0]):
             for y in range(1, field.size[1] + 1):
 
-                if x != 0 and field.field[x - 1][y].patency():
-                    self.graf[x][y].add_new_neighbors(self.graf[x - 1][y])
-                    if y != 0 and field.field[x - 1][y - 1].patency():
-                        self.graf[x][y].add_new_neighbors(self.graf[x - 1][y - 1])
-                    if y != field.size[1] - 1 and field.field[x - 1][y + 1].patency():
-                        self.graf[x][y].add_new_neighbors(self.graf[x - 1][y + 1])
+                if x != 0: 
+                    if field.field[x - 1][y][0].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x - 1][y])
+                    if y != 0: 
+                        if field.field[x - 1][y - 1][0].patency():
+                            self.graf[x][y].add_new_neighbors(self.graf[x - 1][y - 1])
+                    if y != field.size[1] - 1: 
+                        if field.field[x - 1][y + 1][0].patency():
+                            self.graf[x][y].add_new_neighbors(self.graf[x - 1][y + 1])
 
-                if x != field.size[0] - 1 and field.field[x + 1][y].patency():
-                    self.graf[x][y].add_new_neighbors(self.graf[x + 1][y])
-                    if y != 0 and field.field[x + 1][y - 1].patency():
-                        self.graf[x][y].add_new_neighbors(self.graf[x + 1][y - 1])
-                    if y != field.size[1] - 1 and field.field[x + 1][y + 1].patency():
-                        self.graf[x][y].add_new_neighbors(self.graf[x + 1][y + 1])
+                if x != field.size[0] - 1:
+                    if field.field[x + 1][y][0].patency():
+                        self.graf[x][y].add_new_neighbors(self.graf[x + 1][y])
+                    if y != 0: 
+                        if field.field[x + 1][y - 1][0].patency():
+                            self.graf[x][y].add_new_neighbors(self.graf[x + 1][y - 1])
+                    if y != field.size[1] - 1: 
+                        if field.field[x + 1][y + 1][0].patency():
+                            self.graf[x][y].add_new_neighbors(self.graf[x + 1][y + 1])
 
     def way(self, point_start, point_end):
         pass

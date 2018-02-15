b8bea3945fd344b745f439e047d70ca6ef8101ba
ragarg <trall_99@mail.ru>
2018-02-15 11:38:54 +0300

    Update Wall.py

--- Wall.py
+++ Wall.py
@@ -2,10 +2,9 @@ import Object
 
 class Wall(Object.Object):
     def __init__(self, x, y, image):
-        Object.Object.__init__(x, y, image)
+        Object.Object.__init__(self, x, y, image)
         self.size = (24, 24)
         self.patency = 0
 
     def GetSize(self):
         return self.size
-

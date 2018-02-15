200a987f7d2bb23c5e559a2caf011aa129072dff
ragarg <trall_99@mail.ru>
2018-02-15 11:38:23 +0300

    Update Objects.py

--- Objects.py
+++ Objects.py
@@ -1,8 +1,11 @@
-from Object import *
+from Floor import *
+from Hero import Hero
 import pygame
 
 grass_image = pygame.image.load("arts/grass.png")
 dirt_image = pygame.image.load("arts/dirt.png")
+hero_image = pygame.image.load("arts/hero.png")
 
-grass_maket = Object(20, 20, grass_image)
-dirt_maket = Object(25, 27, dirt_image)
+Hero = Hero(20, 20, hero_image)
+grass_maket = Floor(20, 20, grass_image)
+dirt_maket = Floor(25, 27, dirt_image)

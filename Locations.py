600af7a70fb21cf2dc09a902baeead5e155620cb
ragarg <trall_99@mail.ru>
2018-02-15 11:37:43 +0300

    Update Locations.py

--- Locations.py
+++ Locations.py
@@ -1,4 +1,15 @@
 import Location
 from Objects import *
+from Floor import *
+from Wall import *
 
-location_sample = Location.Location(40, 40, [grass_maket, dirt_maket])
+a = []
+grass_image = pygame.image.load("arts/grass.png")
+dirt_image = pygame.image.load("arts/dirt.png")
+for i in range(100):
+    for j in range(100):
+        a.append(Floor(i, j, grass_image))
+for i in range(10, 14):
+    for j in range(10, 14):
+        a.append(Wall(i, j, dirt_image))
+location_sample = Location.Location(100, 100, a, [Hero])

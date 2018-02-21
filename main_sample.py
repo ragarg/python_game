8cece333e0b40fc258151422572bd59d092a1592
ragarg <trall_99@mail.ru>
2018-02-21 20:33:51 +0300

    Update main_sample.py

--- main_sample.py
+++ main_sample.py
@@ -6,6 +6,8 @@ pygame.init()
 size = width, height = 900, 600
 screen = pygame.display.set_mode(size)
 core = Core.Core()
+
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,13 +18,14 @@ while 1:
                 mouse_pos = event.pos
                 core.update_way(mouse_pos)
 
-    core.update_field()
-    
-    for line in core.loc.field:
+    cells = core.update_field()
+
+    for line in cells:
         for obj in line:
             if not obj[0] is None:
                 screen.blit(obj[0].image, (obj[0].x * 24, obj[0].y * 24))
             if not obj[1] is None:
                 screen.blit(obj[1].image, (obj[1].x * 24, obj[1].y * 24))
-    
+
+    pygame.time.wait(100)
     pygame.display.flip()
